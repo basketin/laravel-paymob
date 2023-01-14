ee46c368a6671a2eb5e19a178311ce48a01231f5
Karim Mohamed <komicho1996@gmail.com>
2023-01-14 20:53:48 +0200

    first commit

---
+++ src/PaymobServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace OutMart\Paymob;
+
+use Illuminate\Support\ServiceProvider;
+
+class PaymobServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+}
